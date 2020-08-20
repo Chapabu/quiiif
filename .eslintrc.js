e08bbf8624320870d1d7e341c6b812767977884b
Chapabu <chapabu101@gmail.com>
2020-08-20 03:12:18 +0100

    Tweak ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,8 +23,6 @@ module.exports = {
   rules: {
     '@typescript-eslint/no-explicit-any': 'warn',
     '@typescript-eslint/no-non-null-assertion': 'off',
-    'import/prefer-default-export': 'off',
-    'import/no-default-export': 'error',
     'import/no-unresolved': 'off',
   },
   settings: {
